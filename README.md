c65e65e185217a14528041c165f03b93644935bb
rafarinazze <rafarinazze@gmail.com>
2023-12-18 16:26:47 -0300

    Melhorias no app.py

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Python-Whatsapp-Mensagens-Automatizadas
 Script desenvolvido com referência do tutorial Como Criar um Bot de Mensagens P/ Whatsapp com Python [FÁCIL] do canal Dev Aprender | Jhonatan de Souza
 [https://www.youtube.com/watch?v=gv9dRvkn0rI]
+
+## Dependências
+openpyxl
+pyautogui
+pillow
--- app.py
+++ app.py
@@ -14,26 +14,26 @@ for linha in pagina_clientes.iter_rows(min_row=2):
     nome = linha[0].value
     telefone = linha[1].value
     vencimento = linha[2].value
-    
     mes = data_atual.month
-    
-    mensagem = f'Olá {nome}, seu boleto vence dia {vencimento}/{mes}, acesse https://site.com.br/boleto'
+    hoje = data_atual.day
+    if (vencimento > hoje):
+        print("Teste1")
+        mensagem = f'Olá {nome}, seu boleto vence dia {vencimento}/{mes}, acesse https://site.com.br/boleto'
    
-    try:
-        link_whats = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
-        webbrowser.open(link_whats)
-        sleep(10)
+        try:
+            link_whats = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+            webbrowser.open(link_whats)
+            sleep(7)
+            seta = pyautogui.locateCenterOnScreen('seta.png')
+            sleep(2)
+            pyautogui.click(seta[0],seta[1])
+            sleep(2)
+            pyautogui.hotkey('ctrl', 'w')
+            sleep(2)
+        except:
+            print(f'Envio falhou para {nome}')
+            with open('erros.csv','a+',newline='',encoding='utf-8') as arquivo:
+                arquivo.write(f'\r\n{hoje}/{mes},{nome},{telefone}')
+            pyautogui.hotkey('ctrl', 'w')
+    print("Teste2")
 
-        seta = pyautogui.locateCenterOnScreen('seta.png')
-        sleep(5)
-
-        pyautogui.click(seta[0],seta[1])
-        sleep(2)
-
-        pyautogui.hotkey('ctrl', 'w')
-        sleep(5)
-    except:
-        print(f'Envio falhou para {nome}')
-        with open('erros.csv','a',newline='',encoding='utf-8') as arquivo:
-            arquivo.write(f'{nome},{telefone}')
-        pyautogui.hotkey('ctrl', 'w')
